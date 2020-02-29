7fd93a8de65048847a8bf31d71d735b1d27ca070
evhen <evhen.dytyniak@shaw.ca>
2020-02-29 15:26:26 -0800

    impute function added

---
+++ R/impute.R
@@ -0,0 +1,16 @@
+#' Impute Missing Data
+#'
+#' Automatically imputes missing data by detecting a features class and
+#' comparing test results of multiple models using different imputation methods
+#'
+#' @param data tibble or dataframe including only model features
+#'
+#' @return tibble with imputed features
+#' @export
+#'
+#' @examples
+#' my_data = tibble(...)
+#' imputed_data = impute(my_data)
+impute <- function(data){
+  return()
+}
