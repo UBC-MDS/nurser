c317c8c9127fe1014141a388c9009b6d9133cbba
Elliott Ribner <elliottribner@Elliotts-MacBook-Pro.local>
2020-03-03 20:28:05 -0800

    working

--- R/preproc.R
+++ R/preproc.R
@@ -1,3 +1,6 @@
+library(tidyverse)
+library(dplyr)
+library(fastDummies)
 #' Preproc
 #'
 #' @param X_train (dpylyr dataframe) X_train dateframe - Required
@@ -13,7 +16,25 @@
 #' result = preproc(X_train, X_test)
 #' processed_X_train = result[0]
 #' processed_X_test = result[1]
-  preproc <- function(X_train, X_test=NULL) {
-  X_train
+  preproc <- function(X) {
+    if (!is.data.frame(X)) {
+      stop("X_train must be of type dataframe")
+    }
+    column_types <- sapply(X, class)
+    do_dummies <- FALSE
+    i<-0
+    for (class in column_types) {
+      i<-i+1
+      if (class == "numeric") {
+        X[[i]] <- scale(X[[i]])
+      }
+      if (class == "factor" | class == "character") {
+        do_dummies <- TRUE
+      }
+    }
+    if (do_dummies == TRUE) {
+      X <- fastDummies::dummy_cols(X, remove_first_dummy=TRUE, remove_selected_columns = TRUE)
+    }
+    X
 }
 
